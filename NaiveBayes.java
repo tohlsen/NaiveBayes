9dc29bf9a7f5070d41845089b12d3d77960b9bbc
Tyler Ohlsen <ohlsen.tyler@gmail.com>
2018-01-28 22:16:45 -0800

    Optimized for taking command line arguments & added extensive comments

--- NaiveBayes.java
+++ NaiveBayes.java
@@ -8,40 +8,42 @@ import java.util.Scanner;
 
 public class NaiveBayes {
     
-    /*
-     *  This function reads in a file and returns a 
-     *  set of all the tokens. It ignores the subject line
-     *  
-     *  If the email had the following content:
-     *  
-     *  Subject: Get rid of your student loans
-     *  Hi there ,
-     *  If you work for us, we will give you money
-     *  to repay your student loans . You will be 
-     *  debt free !
-     *  FakePerson_22393
-     *  
-     *  This function would return to you
-     *  [hi, be, student, for, your, rid, we, get, of,
-     *  free, if, you, us, give, !, repay, will, loans,
-     *  work, fakeperson_22393, ,, ., money, there, to, debt]
+    /*	****NAIVE BAYES CLASSIFIER****
+     * CSE 312
+     * Winter 2018
+     * Tyler Ohlsen (tlohlsen)
+     * Last Edited: 01.28.2018
+     * 
+     * Program description:
+     *  Takes in a large database of training files,
+     *  and, using the Naive Baye's Classifier Algorithm, learns 
+     *  whether a certain email is ham or spam base on the words contained
+     *  in the email
      */
     
     public static void main(String[] args) throws IOException {
-        //test
+    	//getting arg from command line:
+    	String dir = args[0];
+
     	//used to store all words in all emails
     	HashSet<String> words = new HashSet<String>();
+    	
+    	//used to store the words for spam & ham emails
     	HashSet<String> spamWords = new HashSet<String>();
     	HashSet<String> hamWords = new HashSet<String>();
-    	HashMap<String, Double> spamProbs = new HashMap<String, Double>();
-    	HashMap<String, Double> hamProbs = new HashMap<String, Double>();
+    	
+    	//used to store the counts of all spam & ham words
     	HashMap<String, Integer> spamCounts = new HashMap<String, Integer>();
     	HashMap<String, Integer> hamCounts = new HashMap<String, Integer>();
     	
-    	//creating files for ham, spam & test emails
-    	File spam = new File("data/train/spam");
-    	File ham = new File("data/train/ham");
-    	File test = new File("data/test");
+    	//used to store the probabilities of all spam & ham words
+    	HashMap<String, Double> spamProbs = new HashMap<String, Double>();
+    	HashMap<String, Double> hamProbs = new HashMap<String, Double>();
+    	
+    	//creating files for ham, spam & test emails (and an array of the files found in their directories)
+    	File spam = new File(dir + "\train\spam");
+    	File ham = new File(dir + "\train\ham");
+    	File test = new File(dir + "\test");
     	File[] spamEmails = spam.listFiles();
     	File[] hamEmails = ham.listFiles();
     	File[] testEmails = test.listFiles();
@@ -51,14 +53,17 @@ public class NaiveBayes {
     	//computing P(H):
     	double probHam = (double) (hamEmails.length) / (spamEmails.length + hamEmails.length);
     	
-    	
-    	//getting all words in spam emails (and getting the word counts)
+    	/*
+    	 * For each spam email:
+    	 * 	- collect all the words within it
+    	 * 	- for each word:
+    	 * 		- increase the count of that word being found in a spam email,
+    	 * 		  or add it & give it a count of 1
+    	 */
     	for (File email : spamEmails) {
     		HashSet<String> spamEmailWords = tokenSet(email);
-    		spamWords.addAll(spamEmailWords); //adding all spam words into general words
-    		//for each word in the entire words thus far:
-    		//if already exists in spamCounts: increase count by 1
-    		//else: put a new one, w/ count of 1
+    		spamWords.addAll(spamEmailWords);
+    		
     		for (String word : spamWords) {
     			if (spamCounts.containsKey(word)) {
     				int oldCount = spamCounts.get(word);
@@ -70,13 +75,17 @@ public class NaiveBayes {
     		}
     		
     	}
-    	//getting all words in ham emails
+    	/*
+    	 * For each ham email:
+    	 * 	- collect all the words within it
+    	 * 	- for each word:
+    	 * 		- increase the count of that word being found in a ham email,
+    	 * 		  or add it & give it a count of 1
+    	 */
     	for (File email : hamEmails) {
     		HashSet<String> hamEmailWords = tokenSet(email);
-    		hamWords.addAll(hamEmailWords); //adding all spam words into general words
-    		//for each word in the entire words thus far:
-    		//if already exists in hamCounts: increase count by 1
-    		//else: put a new one, w/ count of 1
+    		hamWords.addAll(hamEmailWords);
+    		
     		for (String word : hamWords) {
     			if (hamCounts.containsKey(word)) {
     				int oldCount = hamCounts.get(word);
@@ -88,22 +97,34 @@ public class NaiveBayes {
     		}
     	}
     	
+    	//adding both subsets of words to all the words
     	words.addAll(spamWords);
     	words.addAll(hamWords);
     	
+    	//used to hold the probabilities of each word being in a spam or ham email
     	double spamProb, hamProb;
-    	//now that we have counts, find probability of each spam word:
+    	/*
+    	 * for each word in all of the emails:
+    	 * 	- calculate the probability of it being found in a spam or ham email
+    	 * 	- if there's no count of that word: set count to 0
+    	 * 	- add the word and its corresponding ham & spam probabilities to the hashmaps
+    	 */
     	for (String word : words) {
-    		//calculating the probabilities for each word
+    		
+    		//searching for word in spamCounts:
     		if (spamCounts.containsKey(word)) {
     			spamProb = (double) (spamCounts.get(word) + 1) / (spamEmails.length + 2);
     		}
+    		//if not found: set count to 0
     		else {
     			spamProb = (double) 1 / (spamEmails.length + 2);
     		}
+    		
+    		//searching for word in hamCounts:
     		if (hamCounts.containsKey(word)) {
     			hamProb = (double) (hamCounts.get(word) + 1) / (hamEmails.length + 2);
     		}
+    		//if not found: set count to 0
     		else {
     			hamProb = (double) 1 / (hamEmails.length + 2);
     		}
@@ -112,19 +133,19 @@ public class NaiveBayes {
     		spamProbs.put(word, spamProb);
     		hamProbs.put(word, hamProb);
     	}
-    		
-    	
     	
     	/*
     	 * for each test email:
     	 * 	- for each word in the test email:
     	 * 		- if its an analyzed word, add its probability to the running totals of each probability
     	 * 		- whichever probability is greater: print spam or ham
+    	 * NOTE: we use log in these calculations to prevent floating point underflow
     	 */
-    	//used for 
+    	//used for keeping the running total of probabilities of each word
     	double hamSum, spamSum;
     	
     	for (File email : testEmails) {
+    		
     		//resetting their counts after each email
     		hamSum = 0.0;
     		spamSum = 0.0;
@@ -136,16 +157,17 @@ public class NaiveBayes {
     				hamSum += Math.log(hamProbs.get(word));		//adding to the running total of ham probs
     			}
     		}
-    		if ((Math.log(probSpam) + spamSum) > (Math.log(probHam) + hamSum)) { //calculation from notes
-    			System.out.println(email + " spam");
+    		if ((Math.log(probSpam) + spamSum) > (Math.log(probHam) + hamSum)) { //calculation from notes on Naive Baye's algorithm
+    			System.out.println(email.getName() + " spam");	//printing spam if p(S | x_1...x_n) > 0.5
     		}
     		else {
-    			System.out.println(email + " ham");
+    			System.out.println(email.getName() + " ham");	//printing ham if p(H | x_1...x_n) > 0.5
     		}
     	}
     	
     }
     
+    //given
     public static HashSet<String> tokenSet(File filename) throws IOException {
         HashSet<String> tokens = new HashSet<String>();
         Scanner filescan = new Scanner(filename);
